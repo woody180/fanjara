da0177dbc5d71d8ac4f7d2f8210f2dd02e1b84c1
woody180 <woody180@rocketmail.com>
2023-05-14 14:27:02 +0400

    translations

--- app/Languages/en/trans.php
+++ app/Languages/en/trans.php
@@ -8,5 +8,15 @@ return [
     'contact_us' => 'Get in touch',
     'info' => 'Information',
     'products' => 'Products',
-    'add_product' => 'Add product'
+    'add_product' => 'Add product',
+    'title' => 'Title',
+    'url' => 'Url',
+    'price' => 'Price',
+    'priceconstructorurl' => 'Price Constructor url',
+    'content' => 'Content',
+    'thumbnail' => 'Thumbnail',
+    'gallery' => 'Gallery',
+    'send' => 'Send',
+    'create' => 'Create',
+    'submit' => 'Submit',
 ];
