ce6336b98b03acc476a16df505b80521235f8f63
woody180 <woody180@rocketmail.com>
2023-05-15 14:12:10 +0400

    admin product list

--- app/Routes/ProductRoute.php
+++ app/Routes/ProductRoute.php
@@ -8,6 +8,7 @@ $router = Router::getInstance();
 $router->get('product/new',                'ProductController@new');
 $router->post('product',                   'ProductController@create');
 $router->get('product',                    'ProductController@index');
+$router->get('productlist',                'ProductController@proudctList', ['Middlewares/checkAdmin']);
 $router->get('product/(:segment)',         'ProductController@show');
 $router->get('product/(:segment)/edit',    'ProductController@edit');
 $router->put('product/(:segment)',         'ProductController@update');
