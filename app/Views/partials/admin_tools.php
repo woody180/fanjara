db12edbea5da2e35a2cc773ff1de0a12f2f81f50
woody180 <woody180@rocketmail.com>
2023-05-15 14:14:56 +0400

    admin tools

---
+++ app/Views/partials/admin_tools.php
@@ -0,0 +1,6 @@
+<div class="uk-position-fixed uk-position-bottom-left uk-margin-left uk-margin-bottom uk-flex uk-flex-column">
+    
+    <a uk-tooltip="pos: right; title: Products" href="<?= baseUrl("productlist") ?>" uk-icon="icon: cart" class="uk-icon-button uk-box-shadow-large uk-margin-small-bottom"></a>
+    <a uk-tooltip="pos: right; title: Product categories" href="<?= baseUrl("productcategory") ?>" uk-icon="icon: file-text" class="uk-icon-button uk-box-shadow-large"></a>
+    
+</div>
