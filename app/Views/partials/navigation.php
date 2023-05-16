a27cfb98bec843998423f88225f7852fd33b52db
woody180 <woody180@rocketmail.com>
2023-05-16 15:06:23 +0400

    navigation urls

--- app/Views/partials/navigation.php
+++ app/Views/partials/navigation.php
@@ -1,18 +1,16 @@
 <ul class="uk-navbar-nav">
-    <li class="uk-active"><a href="#">მთავარი</a></li>
-    <li><a href="#">ჩვენ შესახებ</a></li>
+    <li class="uk-active"><a href="<?= baseUrl() ?>">მთავარი</a></li>
+    <li><a href="<?= baseUrl("pages/about") ?>">ჩვენ შესახებ</a></li>
     <li>
         <a href="#">კარ-ფანჯრები <span uk-navbar-parent-icon></span> </a>
         <div class="uk-navbar-dropdown">
             <ul class="uk-nav uk-navbar-dropdown-nav">
-                <li><a href="#">ალუმინის</a></li>
-                <li><a href="#">მეტალოპლასტმასის კარ-ფანჯარა REHAU</a></li>
-                <li><a href="#">შემინვის სხვა სისტემები</a></li>
-                <li><a href="#">ინტერიერის ტიხრის სისტემები</a></li>
-                <li><a href="#">მარკიზები და პერგოლა</a></li>
+                <?php foreach (initModel('Productcategory')->list() as $pc): ?>
+                <li><a href="<?= baseUrl("productcategory/{$pc->url}") ?>"><?= $pc->title ?></a></li>
+                <?php endforeach; ?>
             </ul>
         </div>
     </li>
-    <li><a href="#">სიახლეები</a></li>
-    <li><a href="#">კონტაქტი</a></li>
+    <li><a href="<?= baseUrl("news") ?>">სიახლეები</a></li>
+    <li><a href="<?= baseUrl("get-in-touch") ?>">კონტაქტი</a></li>
 </ul>
