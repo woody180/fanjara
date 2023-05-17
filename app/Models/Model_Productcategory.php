90d0db28369ee659ce18b5de918457a0403357bc
woody180 <woody180@rocketmail.com>
2023-05-17 14:31:51 +0400

    categoryProducts method in product category model

--- app/Models/Model_Productcategory.php
+++ app/Models/Model_Productcategory.php
@@ -31,4 +31,28 @@ class Model_Productcategory extends RedBean_SimpleModel {
     {
         return R::find('productcategory', 'order by id desc');
     }
+    
+    
+    public function categoryProducts(string $url)
+    {
+        $category = R::findOne('productcategory', 'url = ?', [$url]) ?? abort();
+        $totalPages = $category->countShared('product');
+        $currentPage = $_GET["page"] ?? 1;
+        if ($currentPage < 1 OR $currentPage > $totalPages) $currentPage = 1;
+        $limit = 12;
+        $offset = ($currentPage - 1) * $limit;  
+        $pagingData = pager([
+            'total' => $totalPages,
+            'limit' => $limit,
+            'current' => $currentPage
+        ]); 
+        //$pages = R::find("pages", "order by timestamp asc limit $limit offset $offset");
+        $product = $category->with("order by id desc limit $limit offset $offset")->sharedProduct;
+        
+        $obj = new stdClass();
+        $obj->pager = $totalPages > $limit ? $pagingData : null;
+        $obj->data = $product;
+        
+        return $obj;
+    }
 }
