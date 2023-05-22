03d5d1130f9a8c91ef539724f07cdc77f31068e4
woody180 <woody180@rocketmail.com>
2023-05-22 11:58:33 +0400

    admin pages list

---
+++ app/Views/admin/pages/list.php
@@ -0,0 +1,44 @@
+<?php $this->layout('partials/template') ?>
+
+
+<?= $this->start('scripts') ?>
+<script src="<?= assetsUrl('tinyeditor/tinyeditor.js') ?>"></script>
+<?= $this->stop() ?>
+
+
+<?= $this->start('mainSection') ?>
+<section class="uk-section">
+    <div class="uk-container min-height">
+        
+        
+        <div class="uk-flex uk-flex-between uk-flex-middle">
+            <h3 class="uk-margin-remove"><?= App\Engine\Libraries\Languages::translate('trans.pages') ?></h3>
+            
+            <a href="<?= baseUrl("page/new") ?>" class="uk-button uk-button-icon uk-button-icon-right uk-button-secondary">
+                <span uk-icon="icon: plus"></span>
+                <span><?= App\Engine\Libraries\Languages::translate('trans.create') ?></span>
+            </a>
+        </div>
+        
+
+        <ul class="uk-list uk-list-striped">
+            <?php foreach ($data->pages as $page): ?>
+            <li>
+                <div class="uk-flex uk-flex-between uk-flex-middle">
+                    <div><?= $page->title ?></div>
+                    
+                    <div class="uk-flex">
+                        <a href="<?= baseUrl("page/{$page->url}") ?>" uk-icon="icon: pencil;"></a>
+                        <form method="POST" uk-icon="icon: trash;">
+                            <?= setMethod('DELETE') ?>
+                        </form>
+                    </div>
+                </div>
+            </li>
+            <?php endforeach; ?>
+        </ul>
+
+
+    </div>
+</section>
+<?= $this->stop(); ?>
