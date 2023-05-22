4f5071ef91fd288985985668e14ba3a295064d03
woody180 <woody180@rocketmail.com>
2023-05-23 03:26:21 +0400

    complete page view

--- app/Views/pages/page.php
+++ app/Views/pages/page.php
@@ -1,5 +1,6 @@
 <?php $this->layout('partials/template', [
-    'title' => $page->title
+    'title' => $page->title,
+    'body_class' => 'init-tinyeditor'
 ]) ?>
 
 
@@ -10,6 +11,38 @@
 
 <?php $this->start('mainSection') ?>
 
-<?= $page->body ?>
+<section class="uk-section">
+    <div class="uk-container">
+        
+        <div>
+            
+            <?php if (checkAuth([1])): ?>
+            <div class="editable" alias="page.<?= $page->id ?>.title">
+            <?php endif; ?>
+                <h1 class="uk-text-secondary editable-cage">
+                    <?= $page->title ?>
+                </h1>
+            <?php if (checkAuth([1])): ?>
+            </div>
+            <?php endif; ?>
+            
+            <div>
+                <hr class="uk-margin-remove">
+            </div>
+        </div>
+        
+        <?php if (checkAuth([1])): ?>
+        <div class="editable" alias="page.<?= $page->id ?>.body">
+            <div class="editable-cage">
+        <?php endif; ?>
+
+        <?= relevantPath($page->body) ?>
+
+        <?php if (checkAuth([1])): ?>
+            </div>
+        </div>
+        <?php endif; ?>
+    </div>
+</section>
 
 <?php $this->stop() ?>
