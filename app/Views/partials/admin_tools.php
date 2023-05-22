acac01cdcbfe787b83da7a86a8205d2f19b431b0
woody180 <woody180@rocketmail.com>
2023-05-22 11:39:13 +0400

    fix page url

--- app/Views/partials/admin_tools.php
+++ app/Views/partials/admin_tools.php
@@ -1,6 +1,7 @@
 <div class="uk-position-fixed uk-position-bottom-left uk-margin-left uk-margin-bottom uk-flex uk-flex-column uk-position-z-index">
     
     <a uk-tooltip="pos: right; title: Products" href="<?= baseUrl("productlist") ?>" uk-icon="icon: cart" class="uk-icon-button uk-box-shadow-large uk-margin-small-bottom"></a>
-    <a uk-tooltip="pos: right; title: Product categories" href="<?= baseUrl("productcategory") ?>" uk-icon="icon: file-text" class="uk-icon-button uk-box-shadow-large"></a>
+    <a uk-tooltip="pos: right; title: Product categories" href="<?= baseUrl("productcategory") ?>" uk-icon="icon: folder" class="uk-icon-button uk-box-shadow-large uk-margin-small-bottom"></a>
+    <a uk-tooltip="pos: right; title: Create page" href="<?= baseUrl("page/new") ?>" uk-icon="icon: file-text" class="uk-icon-button uk-box-shadow-large"></a>
     
 </div>
