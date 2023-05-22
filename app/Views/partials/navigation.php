205ac57eb60cc7b8b3504eca97e5ec1964584bae
woody180 <woody180@rocketmail.com>
2023-05-22 11:29:36 +0400

    update navigation

--- app/Views/partials/navigation.php
+++ app/Views/partials/navigation.php
@@ -1,8 +1,7 @@
 <ul class="uk-navbar-nav">
     <li class="uk-active"><a href="<?= baseUrl() ?>">მთავარი</a></li>
-    <li><a href="<?= baseUrl("pages/about") ?>">ჩვენ შესახებ</a></li>
     <li>
-        <a href="#">კარ-ფანჯრები <span uk-navbar-parent-icon></span> </a>
+        <a href="#">პროდუქცია <span uk-navbar-parent-icon></span> </a>
         <div class="uk-navbar-dropdown">
             <ul class="uk-nav uk-navbar-dropdown-nav">
                 <?php foreach (initModel('Productcategory')->list() as $pc): ?>
@@ -11,6 +10,22 @@
             </ul>
         </div>
     </li>
-    <li><a href="<?= baseUrl("news") ?>">სიახლეები</a></li>
-    <li><a href="<?= baseUrl("get-in-touch") ?>">კონტაქტი</a></li>
+    <li><a href="<?= baseUrl("get-in-touch") ?>">სერვისი <span uk-navbar-parent-icon></span> </a>
+        <div class="uk-navbar-dropdown">
+            <ul class="uk-nav uk-navbar-dropdown-nav">
+                <li><a href="<?= baseUrl("page/consulting") ?>">კონსულტაცია</a></li>
+                <li><a href="<?= baseUrl("page/projects") ?>">პროექტები</a></li>
+            </ul>
+        </div>
+    </li>
+    <li><a href="<?= baseUrl("get-in-touch") ?>">კომპანია <span uk-navbar-parent-icon></span></a>
+        <div class="uk-navbar-dropdown">
+            <ul class="uk-nav uk-navbar-dropdown-nav">
+                <li><a href="<?= baseUrl("page/about") ?>">ჩვენს შესახებ</a></li>
+                <li><a href="<?= baseUrl("page/vacancies") ?>">ვაკანსია</a></li>
+                <li><a href="<?= baseUrl("page/partners") ?>">პარტნიორები</a></li>
+            </ul>
+        </div>
+    </li>
+    <li class="uk-active"><a href="<?= baseUrl("page/contact") ?>">კონტაქტი</a></li>
 </ul>
