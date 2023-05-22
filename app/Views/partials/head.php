51cb5ab82eda02b91d5817e4f24034e5b464472d
woody180 <woody180@rocketmail.com>
2023-05-23 02:38:00 +0400

    add language to HTML

--- app/Views/partials/head.php
+++ app/Views/partials/head.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="<?= App\Engine\Libraries\Languages::active() ?>">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
