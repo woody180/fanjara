b0cbb006663552daae40eeefb451f2b386a8b20b
woody180 <woody180@rocketmail.com>
2023-05-22 14:00:10 +0400

    change positions

--- app/Views/partials/admin_tools.php
+++ app/Views/partials/admin_tools.php
@@ -1,7 +1,7 @@
 <div class="uk-position-fixed uk-position-bottom-left uk-margin-left uk-margin-bottom uk-flex uk-flex-column uk-position-z-index">
     
-    <a uk-tooltip="pos: right; title: Products" href="<?= baseUrl("productlist") ?>" uk-icon="icon: cart" class="uk-icon-button uk-box-shadow-large uk-margin-small-bottom"></a>
+    <a uk-tooltip="pos: right; title: Products list" href="<?= baseUrl("productlist") ?>" uk-icon="icon: cart" class="uk-icon-button uk-box-shadow-large uk-margin-small-bottom"></a>
+    <a uk-tooltip="pos: right; title: Pages list" href="<?= baseUrl("page") ?>" uk-icon="icon: file-text" class="uk-icon-button uk-box-shadow-large uk-margin-small-bottom"></a>
     <a uk-tooltip="pos: right; title: Product categories" href="<?= baseUrl("productcategory") ?>" uk-icon="icon: folder" class="uk-icon-button uk-box-shadow-large uk-margin-small-bottom"></a>
-    <a uk-tooltip="pos: right; title: Create page" href="<?= baseUrl("page/index") ?>" uk-icon="icon: file-text" class="uk-icon-button uk-box-shadow-large"></a>
     
 </div>
