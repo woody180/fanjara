589ebcb8bc671c4639855c3e85d8fde60fc4014b
woody180 <woody180@rocketmail.com>
2023-05-22 11:40:30 +0400

    change admin page url

--- app/Views/partials/admin_tools.php
+++ app/Views/partials/admin_tools.php
@@ -2,6 +2,6 @@
     
     <a uk-tooltip="pos: right; title: Products" href="<?= baseUrl("productlist") ?>" uk-icon="icon: cart" class="uk-icon-button uk-box-shadow-large uk-margin-small-bottom"></a>
     <a uk-tooltip="pos: right; title: Product categories" href="<?= baseUrl("productcategory") ?>" uk-icon="icon: folder" class="uk-icon-button uk-box-shadow-large uk-margin-small-bottom"></a>
-    <a uk-tooltip="pos: right; title: Create page" href="<?= baseUrl("page/new") ?>" uk-icon="icon: file-text" class="uk-icon-button uk-box-shadow-large"></a>
+    <a uk-tooltip="pos: right; title: Create page" href="<?= baseUrl("page/index") ?>" uk-icon="icon: file-text" class="uk-icon-button uk-box-shadow-large"></a>
     
 </div>
