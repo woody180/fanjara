96da00ef3e8e98e6c7e51453a558026e3a269647
woody180 <woody180@rocketmail.com>
2023-05-22 11:47:35 +0400

    set middlewares

--- app/Routes/ProductRoute.php
+++ app/Routes/ProductRoute.php
@@ -5,13 +5,13 @@ use App\Engine\Libraries\Router;
 
 $router = Router::getInstance();
 
-$router->get('product/new',                'ProductController@new');
-$router->post('product',                   'ProductController@create');
+$router->get('product/new',                'ProductController@new', ['Middlewares/checkAdmin']);
+$router->post('product',                   'ProductController@create', ['Middlewares/checkAdmin']);
 $router->get('product',                    'ProductController@index');
 $router->get('productlist',                'ProductController@proudctList', ['Middlewares/checkAdmin']);
 $router->get('product/(:segment)',         'ProductController@show');
-$router->get('product/(:segment)/edit',    'ProductController@edit');
-$router->put('product/(:segment)',         'ProductController@update');
-$router->patch('product/(:segment)',       'ProductController@update');
-$router->delete('product/(:segment)',      'ProductController@delete');
+$router->get('product/(:segment)/edit',    'ProductController@edit', ['Middlewares/checkAdmin']);
+$router->put('product/(:segment)',         'ProductController@update', ['Middlewares/checkAdmin']);
+$router->patch('product/(:segment)',       'ProductController@update', ['Middlewares/checkAdmin']);
+$router->delete('product/(:segment)',      'ProductController@delete', ['Middlewares/checkAdmin']);
         
