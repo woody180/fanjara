b7b61215feeeeb00252b4e1b959798f34205c469
woody180 <woody180@rocketmail.com>
2023-05-22 12:37:11 +0400

    page model save method

---
+++ app/Models/Model_Page.php
@@ -0,0 +1,90 @@
+<?php
+        
+class Model_Page extends RedBean_SimpleModel {
+
+    public function open() {
+        
+    }
+
+    public function dispense() {
+        
+    }
+
+    public function update() {
+        
+    }
+
+    public function after_update() {
+        
+    }
+
+    public function delete() {
+        
+    }
+
+    public function after_delete() {
+        
+    }
+
+
+    public function migrate()
+    {
+        $page = R::dispense('page');
+        $page->import([
+            'title' => 'title',
+            'url' => 'url',
+            'description' => 'description',
+            'thumbnail' => 'thumbnail',
+            'body' => 'body',
+            'createdat' => 'createdat'
+        ]);
+        R::store($page);
+    }
+    
+    
+    
+    public function getPage($url)
+    {
+        if (is_string($url)) return R::findOne ('page', 'url = ?', [$url]) ?? abort();
+        return R::findOne('page', 'id = ?', [$url]) ?? abort();
+    }
+    
+    
+    public function list()
+    {
+        $totalPages = R::count('page');
+        $currentPage = $_GET["page"] ?? 1;
+        if ($currentPage < 1 OR $currentPage > $totalPages) $currentPage = 1;
+        $limit = 12;
+        $offset = ($currentPage - 1) * $limit;  
+        $pagingData = pager([
+            'total' => $totalPages,
+            'limit' => $limit,
+            'current' => $currentPage
+        ]); 
+        $pages = R::find("page", "order by id desc limit $limit offset $offset");
+        
+        $obj = new stdClass();
+        $obj->pager = $totalPages > $limit ? $pagingData : null;
+        $obj->pages = $pages;
+        
+        return $obj;
+    }
+    
+    
+    
+    public function save($body)
+    {
+        $slugify = new Cocur\Slugify\Slugify();    
+        
+        $page_placeholder = file_get_contents(APPROOT . "/Views/admin/pages/page_body_placeholder.html");
+        
+        $page = R::dispense('page');
+        $page->title = $body['title'];
+        $page->url = empty($body['url']) ? $slugify->slugify($body['title']) :  $slugify->slugify($body['url']);
+        $page->description = $body['description'];
+        $page->body = file_get_contents(APPROOT . "/Views/admin/pages/page_body_placeholder.html");
+        
+        return R::store($page); // Returns page ID
+    }
+}
