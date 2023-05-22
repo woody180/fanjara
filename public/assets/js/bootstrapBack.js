2512bef3ab577541edcb38cfc65876bae4fce15b
woody180 <woody180@rocketmail.com>
2023-05-22 13:34:47 +0400

    add tinyeditor initialization

--- public/assets/js/bootstrapBack.js
+++ public/assets/js/bootstrapBack.js
@@ -1,12 +1,15 @@
 const baseurl = document.querySelector('meta[name="baseurl"]') ? document.querySelector('meta[name="baseurl"]').getAttribute('content') : alert('Add <meta name="baseurl" content="website-url" /> baseurl meta tag!');
 
-new FgTinyEditor({
-    selector: '.editable',
-    rootPath: baseurl + '/assets/tinyeditor',
-    saveUrl: baseurl + '/' + 'save',
-    defaultTools: true, // Default is true
-    loadjQuery: true
-});
+
+if (document.body.classList.contains('load-tinyeditor')) {
+    new FgTinyEditor({
+        selector: '.editable',
+        rootPath: baseurl + '/assets/tinyeditor',
+        saveUrl: baseurl + '/' + 'save',
+        defaultTools: true, // Default is true
+        loadjQuery: true
+    });
+}
 
 
 import ProductController from './Controllers/ProductController.js';
