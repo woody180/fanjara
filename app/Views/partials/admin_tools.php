ac66585a169523c268628c09872ad9fca720453d
woody180 <woody180@rocketmail.com>
2023-05-23 18:34:23 +0400

    comment line in admin_tools partial

--- app/Views/partials/admin_tools.php
+++ app/Views/partials/admin_tools.php
@@ -2,6 +2,6 @@
     
     <a uk-tooltip="pos: right; title: Products list" href="<?= baseUrl("productlist") ?>" uk-icon="icon: cart" class="uk-icon-button uk-box-shadow-large uk-margin-small-bottom"></a>
     <a uk-tooltip="pos: right; title: Pages list" href="<?= baseUrl("page") ?>" uk-icon="icon: file-text" class="uk-icon-button uk-box-shadow-large uk-margin-small-bottom"></a>
-    <a uk-tooltip="pos: right; title: Product categories" href="<?= baseUrl("productcategory") ?>" uk-icon="icon: folder" class="uk-icon-button uk-box-shadow-large uk-margin-small-bottom"></a>
+    <!--<a uk-tooltip="pos: right; title: Product categories" href="<?= ''//baseUrl("productcategory") ?>" uk-icon="icon: folder" class="uk-icon-button uk-box-shadow-large uk-margin-small-bottom"></a>-->
     
 </div>
