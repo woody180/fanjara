4c6105fc92375ab3ef5103033549661f0a411c70
woody180 <woody180@rocketmail.com>
2023-05-23 18:32:16 +0400

    mobile lang switcher

--- app/Views/partials/mobileNav.php
+++ app/Views/partials/mobileNav.php
@@ -6,5 +6,11 @@
         <div id="fg-mobile-nav">
             <?= $this->insert('partials/navigation') ?>
         </div>
+        
+        <div class="uk-margin-medium-top uk-text-uppercase">
+            <a href="<?= URLROOT . '/ge' ?>" class="uk-icon-button uk-margin-small-right">ge</a>
+            <a href="<?= URLROOT . '/ru' ?>" class="uk-icon-button uk-margin-small-right">ru</a>
+            <a href="<?= URLROOT . '/en' ?>" class="uk-icon-button">en</a>
+        </div>
     </div>
 </div>
