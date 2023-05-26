cd68514f1bad17dfef969e33cadf4dc3e34f861e
woody180 <woody180@rocketmail.com>
2023-05-26 16:20:46 +0400

    slider routes, set middlewares

---
+++ app/Routes/SliderRoute.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\Engine\Libraries\Router;
+
+
+$router = Router::getInstance();
+
+$router->get('slider/new',                'SliderController@new', ['Middlewares/checkAdmin']);
+$router->post('slider',                   'SliderController@create', ['Middlewares/checkAdmin']);
+$router->get('slider',                    'SliderController@index');
+$router->get('slider/(:segment)',         'SliderController@show');
+$router->get('slider/(:segment)/edit',    'SliderController@edit', ['Middlewares/checkAdmin']);
+$router->put('slider/(:segment)',         'SliderController@update', ['Middlewares/checkAdmin']);
+$router->patch('slider/(:segment)',       'SliderController@update', ['Middlewares/checkAdmin']);
+$router->delete('slider/(:segment)',      'SliderController@delete', ['Middlewares/checkAdmin']);
+        
