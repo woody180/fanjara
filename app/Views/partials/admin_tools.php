5d3391077d0893ae2292e98fae3cb47a39d18b9f
woody180 <woody180@rocketmail.com>
2023-05-26 16:19:49 +0400

    slider admin tool

--- app/Views/partials/admin_tools.php
+++ app/Views/partials/admin_tools.php
@@ -4,5 +4,6 @@
     <a uk-tooltip="pos: right; title: Product categories" href="<?= baseUrl("productcategory/list") ?>" uk-icon="icon: folder" class="uk-icon-button uk-box-shadow-large uk-margin-small-bottom"></a>
     <a uk-tooltip="pos: right; title: Pages list" href="<?= baseUrl("page") ?>" uk-icon="icon: file-text" class="uk-icon-button uk-box-shadow-large uk-margin-small-bottom"></a>
     <a uk-tooltip="pos: right; title: Add project" href="<?= baseUrl("projects/new") ?>" uk-icon="icon: thumbnails" class="uk-icon-button uk-box-shadow-large uk-margin-small-bottom"></a>
+    <a uk-tooltip="pos: right; title: Slides" href="<?= baseUrl("slider") ?>" uk-icon="icon: image" class="uk-icon-button uk-box-shadow-large uk-margin-small-bottom"></a>
     
 </div>
