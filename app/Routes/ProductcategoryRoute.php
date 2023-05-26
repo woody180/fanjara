2abe986336839864622c4595c46d34f920d68846
woody180 <woody180@rocketmail.com>
2023-05-26 13:47:50 +0400

    set middlewares

--- app/Routes/ProductcategoryRoute.php
+++ app/Routes/ProductcategoryRoute.php
@@ -5,12 +5,13 @@ use App\Engine\Libraries\Router;
 
 $router = Router::getInstance();
 
-$router->get('productcategory/new',                'ProductcategoryController@new');
-$router->post('productcategory',                   'ProductcategoryController@create');
+$router->get('productcategory/list',               'ProductcategoryController@list', ['Middlewares/checkAdmin']);
+$router->get('productcategory/new',                'ProductcategoryController@new', ['Middlewares/checkAdmin']);
+$router->post('productcategory',                   'ProductcategoryController@create', ['Middlewares/checkAdmin']);
 $router->get('productcategory',                    'ProductcategoryController@index');
 $router->get('productcategory/(:segment)',         'ProductcategoryController@show');
-$router->get('productcategory/(:segment)/edit',    'ProductcategoryController@edit');
-$router->put('productcategory/(:segment)',         'ProductcategoryController@update');
-$router->patch('productcategory/(:segment)',       'ProductcategoryController@update');
-$router->delete('productcategory/(:segment)',      'ProductcategoryController@delete');
+$router->get('productcategory/(:segment)/edit',    'ProductcategoryController@edit', ['Middlewares/checkAdmin']);
+$router->put('productcategory/(:segment)',         'ProductcategoryController@update', ['Middlewares/checkAdmin']);
+$router->patch('productcategory/(:segment)',       'ProductcategoryController@update', ['Middlewares/checkAdmin']);
+$router->delete('productcategory/(:segment)',      'ProductcategoryController@delete', ['Middlewares/checkAdmin']);
         
