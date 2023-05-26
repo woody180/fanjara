a1c451e6fe39d8a6268aa23395988fc662f2e615
woody180 <woody180@rocketmail.com>
2023-05-26 13:52:05 +0400

    add product categories button

--- app/Views/partials/admin_tools.php
+++ app/Views/partials/admin_tools.php
@@ -1,7 +1,8 @@
 <div class="uk-position-fixed uk-position-bottom-left uk-margin-left uk-margin-bottom uk-flex uk-flex-column uk-position-z-index">
     
     <a uk-tooltip="pos: right; title: Products list" href="<?= baseUrl("productlist") ?>" uk-icon="icon: cart" class="uk-icon-button uk-box-shadow-large uk-margin-small-bottom"></a>
+    <a uk-tooltip="pos: right; title: Product categories" href="<?= baseUrl("productcategory/list") ?>" uk-icon="icon: folder" class="uk-icon-button uk-box-shadow-large uk-margin-small-bottom"></a>
     <a uk-tooltip="pos: right; title: Pages list" href="<?= baseUrl("page") ?>" uk-icon="icon: file-text" class="uk-icon-button uk-box-shadow-large uk-margin-small-bottom"></a>
-    <a uk-tooltip="pos: right; title: Add project" href="<?= baseUrl("projects/new") ?>" uk-icon="icon: folder" class="uk-icon-button uk-box-shadow-large uk-margin-small-bottom"></a>
+    <a uk-tooltip="pos: right; title: Add project" href="<?= baseUrl("projects/new") ?>" uk-icon="icon: thumbnails" class="uk-icon-button uk-box-shadow-large uk-margin-small-bottom"></a>
     
 </div>
