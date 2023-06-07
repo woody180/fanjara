1d4dcd22173bc4c212c9f368260362bc44c1dab9
woody180 <woody180@rocketmail.com>
2023-06-07 14:54:17 +0400

    update welcome view

--- app/Views/welcome.php
+++ app/Views/welcome.php
@@ -30,38 +30,11 @@
 
 <section class="uk-section">
     <div class="uk-container">
-        
-        <div class="uk-text-center uk-text-small uk-card uk-card-body uk-border-rounded uk-width-3-4@m uk-margin-auto">
-            <div class="uk-margin-medium-bottom">
-                <h1>
-                    <?= App\Engine\Libraries\Languages::translate([
-                        'ge' => 'ჩვენ შესახებ',
-                        'en' => 'About us',
-                        'ru' => 'О нас',
-                    ]) ?>
-                </h1>
-                <hr class="uk-divider-small">
-                <p>
-                    <?= App\Engine\Libraries\Languages::translate([
-                        'ge' => 'კომპანია “ა-ვე ლუქსი” პროფესიონალთა გუნდმა დააარსა. აველუქსმა ბინა ვაკეში, კეკელიძის No2-ში დაიდო. კომპანიამ პირველივე დღიდან აქცენტი ისეთი მაღალხარისხიანი პროდუქციის წარმოებაზე გააკეთა, როგორიცაა გერმანული მეტალოპლასმასის კარ-ფანჯარა რეჰაუ. 2009 წელს კომპანიას მიენიჭა REHAU-ს ლიცენზირებული მწარმოებლის სტატუსი (#63762901), რაც იმას ნიშნავს რომ აქ ვერ შეხვდებით ფალსიფიკაციას. აბსოლუტურად ყველა ფანჯარა კომპლექტდება REHAU-ს 1,5 ან 2,0 მმ-იანი არმირებით და REHAU-ს შემამჭიდროვებლებით (რეზინით), რომელიც უნიკალური ტექნოლოგიის წყალობით, მაქსიმალური ჰერმეტულობის გარანტიაა.',
-                        'en' => 'The company "A-ve Lux" was founded by a team of professionals. Avelux placed an apartment in Vake, Kekelidze No. 2. From the very first day, the company focused on the production of such high-quality products as German metalloplasma door-window Rehau. In 2009, the company was granted the status of REHAU licensed manufacturer (#63762901), which means that you will not encounter counterfeits here. Absolutely all windows are equipped with REHAU 1.5 or 2.0 mm reinforcements and REHAU sealings (rubber), which, thanks to the unique technology, guarantee maximum tightness.',
-                        'ru' => 'Компания «А-ве Люкс» была основана командой профессионалов. Avelux разместил квартиру в Ваке, Кекелидзе №2. С самого первого дня компания сосредоточилась на выпуске такой качественной продукции, как немецкие металлоплазменные двери-окна Rehau. В 2009 году компания получила статус лицензированного производителя REHAU (#63762901), а это значит, что с подделками вы здесь не столкнетесь. Абсолютно все окна оснащены армированием REHAU 1,5 или 2,0 мм и уплотнителями REHAU (резиновыми), которые благодаря уникальной технологии гарантируют максимальную герметичность.',
-                    ]) ?>
-                </p>
-                
-                <div class="uk-flex uk-flex-center">
-                    <a href="<?= baseUrl("page/about") ?>" class="uk-button-icon uk-button-icon-right">
-                        <span uk-icon="icon: arrow-right;"></span>
-                        <span><?= App\Engine\Libraries\Languages::translate('trans.read_more') ?></span>
-                    </a>
-                </div>
-            </div>
-        </div>
 
         <!--<div class="fj-category-grid">-->
         <div class="uk-grid-match uk-grid-medium" uk-grid>
             
-            <div class="uk-width-1-2@s">
+            <div class="uk-width-2-3@m uk-width-1-2@s">
                 <div class="uk-card uk-card-body uk-height-medium uk-position-responsive uk-border-rounde uk-overflow-hidden uk-light uk-overflow-hidden uk-border-rounded" data-bg-color="#F2F2F2" data-bg="<?= assetsUrl("tinyeditor/filemanager/files/{$categories[0]->banner}") ?>" bg-size="cover" bg-position="bottom right">
                     <a href="<?= baseUrl("productcategory/{$categories[0]->url}") ?>" class="layer-film"></a>
                     
@@ -76,7 +49,7 @@
                 </div>
             </div>
             
-            <div class="uk-width-1-2@s">
+            <div class="uk-width-1-3@m uk-width-1-2@s">
                 <div class="uk-card uk-card-body uk-height-medium uk-position-responsive uk-border-rounde uk-overflow-hidden uk-light uk-overflow-hidden uk-border-rounded" data-bg-color="#F2F2F2" data-bg="<?= assetsUrl("tinyeditor/filemanager/files/{$categories[4]->banner}") ?>" bg-size="cover" bg-position="bottom right">
                     <a href="<?= baseUrl("productcategory/{$categories[4]->url}") ?>" class="layer-film"></a>
                     
@@ -155,8 +128,17 @@
     <div class="layer-film"></div>
     <div class="uk-container">
         <div class="uk-width-1-2@m uk-text-center uk-margin-auto uk-margin-auto-vertical uk-position-relative uk-position-z-index">
-            <h1>Headline</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas, placeat, odit, quaerat id dolor minima consectetur aut fugiat delectus tenetur ratione blanditiis provident vitae modi suscipit quidem doloremque porro nihil!</p>
+            <?= App\Engine\Libraries\Languages::translate([
+                'ge' => 'ყოველი ჩვენგანი ხომ გამორჩეულია, განსხვავებულია პრიორიტეტებით, თავისი ოცნებებით და სურვილებით.
+ასეთივე ნაირფეროვანია ჩვენთან არჩევანი.
+მოგვეცეთ საშუალება თქვენი სახლისგან მეტი კომფორტის და სიამოვნების მიღება შთაგაგონოთ.',
+                'en' => 'After all, each of us is unique, has different priorities, dreams and desires.
+Our selection is just as diverse.
+Let us inspire you to get more comfort and pleasure from your home.',
+                'ru' => 'Ведь каждый из нас уникален, имеет разные приоритеты, мечты и желания.
+Наш ассортимент такой же разнообразный.
+Позвольте нам вдохновить вас на получение большего комфорта и удовольствия от вашего дома.'
+            ]) ?>
         </div>
     </div>
 
