30d22e8174be223c32b5c1d9a94428b19f0b209e
woody180 <woody180@rocketmail.com>
2023-06-18 12:00:33 +0400

    update google map

--- app/Views/contact.php
+++ app/Views/contact.php
@@ -26,7 +26,7 @@
 
                     <hr class="uk-divider-icon uk-width-medium uk-margin-auto">
 
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2977.168778950643!2d44.78094669284393!3d41.73845227220213!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40447294d9ac56d5%3A0x64f0df4884501d3!2s87%20Akaki%20Tsereteli%20Ave%2C%20T&#39;bilisi!5e0!3m2!1sen!2sge!4v1684833557005!5m2!1sen!2sge" width="100%" height="250" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m13!1m8!1m3!1d1488.5581333987327!2d44.782359!3d41.739585!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNDHCsDQ0JzIyLjUiTiA0NMKwNDYnNTYuNSJF!5e0!3m2!1sen!2sge!4v1687075211986!5m2!1sen!2sge" width="100%" height="250" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                 </div>
 
 
