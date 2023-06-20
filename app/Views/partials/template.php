904b0eb361ba9fba0a658065a1cbec1468f6a3ec
lacrimosakh <lacrimosakh@gmail.com>
2023-06-20 10:54:26 +0400

    container to template

--- app/Views/partials/template.php
+++ app/Views/partials/template.php
@@ -1,5 +1,7 @@
 <?php include APPROOT . '/Views/partials/head.php' ?>
 
-<?= $this->section('mainSection'); ?>
+<div data-style="padding-top: 132px">
+    <?= $this->section('mainSection'); ?>
+</div>
 
 <?php include APPROOT . '/Views/partials/footer.php' ?>
