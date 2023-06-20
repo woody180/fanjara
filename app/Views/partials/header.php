d6c4fec068552aefe0dead7ae41387d6bc84ca99
lacrimosakh <lacrimosakh@gmail.com>
2023-06-20 10:54:14 +0400

    refactoring header

--- app/Views/partials/header.php
+++ app/Views/partials/header.php
@@ -1,36 +1,36 @@
-<div id="main-header" uk-sticky>
+<div id="main-header" class="uk-position-fixed uk-width-1-1 uk-position-z-index" data-style="z-index: 999">
     <div data-style="padding: 6px 0; background-color: #f3f3f3; height: 40px;" class="uk-text-small uk-flex uk-flex-middle uk-visible@m">
         <div class="uk-container uk-flex-1">
             <div uk-grid class="uk-child-width-1-2 uk-grid-collapse">
                 <div class="uk-flex uk-flex-middle">
-                    <div>
+                    <div data-style="font-size: 15px">
                         <?= App\Engine\Libraries\Languages::translate([
                             'ge' => 'თბილისი, წერეთლის 87ზ',
-                            'en' => 'Tbilisi, Tsereteli 87Z',
+                            'en' => 'Tbilisi, Tsereteli 87z',
                             'ru' => 'Тбилиси, Церетели 87з',
                         ]) ?>
                     </div>
                 </div>
 
                 <div>
-                    <div class="uk-flex uk-flex-right uk-flex-middle">
+                    <div class="uk-flex uk-flex-right uk-flex-middle" data-style="font-size: 15px">
                         
                         <a uk-toggle="target: #call-request" href="#" class="uk-text-bold uk-link-reset uk-margin-right"><?= App\Engine\Libraries\Languages::translate(['ge' => 'მოითხოვე ზარი', 'en' => 'Request Call', 'ru' => 'Заказать звонок']) ?></a>
                         
-                        <a href="tel:+995322238060" class="uk-link-reset uk-text-bold">+995 (32) 223 80 60</a>
+                        <a href="tel:+995322238060" class="uk-text-bold fj-phone-number" data-style="color: red;">+995 (32) 223 80 60</a>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 
-    <div class="uk-background-default uk-box-shadow-medium" data-responsive="max-width[980]; style[padding: 15px 0]">
+    <div id="second-header" class="uk-background-default uk-box-shadow-medium uk-animation-reverse" data-responsive="max-width[980]; style[padding: 15px 0]">
         <div class="uk-container">
             <nav uk-navbar>
 
                 <div class="uk-navbar-left">
                     <a href="<?= baseUrl() ?>" class="uk-display-block">
-                        <img src="<?= assetsUrl("images/logo/avelux-logo.png") ?>" alt="<?= APPNAME ?>" width="300" />
+                        <img src="<?= assetsUrl("images/logo/avelux-logo_{$_SESSION['lang']}.png") ?>" alt="<?= APPNAME ?>" width="300" />
                     </a>
                 </div>
 
