49ed4d061f17a6f7268200ea740096d2a4476442
lacrimosakh <lacrimosakh@gmail.com>
2023-06-20 10:53:42 +0400

    google map

--- app/Views/partials/contact.php
+++ app/Views/partials/contact.php
@@ -77,7 +77,7 @@
             
             <div class="uk-width-2-3@m uk-width-1-2@s uk-width-1-1">
                 <div class="uk-overflow-hidden uk-border-rounded">
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m13!1m8!1m3!1d1488.5581333987327!2d44.782359!3d41.739585!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNDHCsDQ0JzIyLjUiTiA0NMKwNDYnNTYuNSJF!5e0!3m2!1ska!2sge!4v1687074950875!5m2!1ska!2sge" width="100%" height="440" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d744.276541047929!2d44.781748570133765!3d41.73980291814836!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNDHCsDQ0JzIzLjIiTiA0NMKwNDYnNTUuNCJF!5e0!3m2!1sen!2sge!4v1687174192870!5m2!1sen!2sge" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                 </div>
             </div>
         </div>
